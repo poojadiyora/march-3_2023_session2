dd59dbd7782adc35ce26f3b697b9f32a77a48b1d
poojadiyora <poojadiyora07@gmail.com>
2023-03-30 22:07:32 -0400

    3rdcommit

---
+++ src/main/java/selenium_secondclass/gittest2.java
@@ -0,0 +1,21 @@
+package selenium_secondclass;
+
+public class gittest2 {
+
+	public static void main(String[] args) {
+		 printme();
+		 printme2();
+		 
+	}
+	public static void printme() {
+		System.out.println("git test");
+	}
+	public static void printme2() {
+		System.out.println("git test");
+		System.out.println("git test2");
+		System.out.println("git test3");
+		System.out.println("git test4");
+		System.out.println("git test5");
+	}
+	
+}
